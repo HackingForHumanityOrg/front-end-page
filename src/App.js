c0e7a8a5f4483c7307e678c3ee8d81cd292f950a
amychan331@yahoo.com <amychan331@yahoo.com>
2017-07-09 00:10:56 -0700

    Got the fetch() working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
+import { FormGroup, InputGroup, FormControl, Glyphicon } from 'react-bootstrap';
+import LocationList from './LocationList';
 import './App.css';
-import Form from './Form';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitted: false,
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({submitted: true});
+
+  }
+
   render() {
-    return (
-      <main>
-        <header>
-          <h2>Wellness For All</h2>
-          <p className="intro">Find a women's health service near you!</p>
-        </header>
-        <Form />
-      </main>
-    );
+    if (this.state.submitted) {
+      return (
+        <main>
+          <LocationList address={this.state.value} />
+        </main>
+      );
+    } else {
+      return (
+        <main>
+          <header>
+            <h2>Wellness For All</h2>
+            <p>Find a women's health service near you!</p>
+          </header>
+          <form onSubmit={this.handleSubmit} className="center-block">
+          <FormGroup>
+            <InputGroup>
+              <FormControl
+                type="text"
+                value={this.state.value}
+                placeholder="Address"
+                onChange={this.handleChange}/>
+              <InputGroup.Addon>
+                <Glyphicon glyph="map-marker" />
+              </InputGroup.Addon>
+            </InputGroup>
+          </FormGroup>
+          </form>
+        </main>
+      );
+    }
   }
 }
 
--- src/LocationList.js
+++ src/LocationList.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem, Well } from 'react-bootstrap';
+import { Jumbotron, Well } from 'react-bootstrap';
 
-function LocationList(props) {
+class LocationList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {location: []};
+  }
+
+  componentDidMount() {
+    this.ListData();
+  }
+
+  ListData() {
+    return fetch(`https://data.sfgov.org/resource/sci7-7q9i.json?facility_type='Free Clinic'`, {
+      method: 'GET',
+    }).then(res => {
+      return res.json()
+    }).then (data => {
+      this.setState({location: data})
+    }).catch(e => {
+      this.setState({location: 'Request Failed: ' + e})
+    });
+  }
+
+  render() {
+    const locations = this.state.location.map((item, i) => {
+      return (
+        <Well key={i}>
+          <h3>{item.facility_name}</h3>
+          <span>{item.facility_type}</span>
+        </Well>)
+    });
+
+    return (
+      <main>
+        <Header />
+        <section>{ locations }</section>
+      </main>
+    );
+  }
+}
+
+function Header() {
   return (
-    <section className="center-block">
-      <ListGroup>
-        <ListGroupItem bsStyle="info">{props.address}</ListGroupItem>
-      </ListGroup>
-      <Well>Look I'm in a well!</Well>
-    </section>
+    <div id="banner">
+      <section id="search-engine">Test</section>
+      <section id="login-register">Test</section>
+    </div>
   );
 }
+
 
 export default LocationList;
